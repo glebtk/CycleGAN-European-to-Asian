62a1c8f1947f6a94d94e0f793cbe2c217d78c98e
Gleb Tutik <tutikgv@gmail.com>
2023-04-26 19:43:27 +0600

    Add set_seed function

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import sys
 import torch
+import random
 import numpy as np
 
 from transforms import Transforms
@@ -96,3 +97,15 @@ def postprocessing(tensor, config):
         image[channel] *= 255
 
     return image.astype('uint8')
+
+
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+    print(f"Random seed set as {seed}")
